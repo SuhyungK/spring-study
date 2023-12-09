05e55b14855a72bebfcc174f8b8dc258cdd00018
SuhyungK <dook4966@gmail.com>
2023-12-09 20:12:54 +0900

    섹션3. AppConfig 리팩토링

--- core-study/src/main/java/com/example/corestudy/AppConfig.java
+++ core-study/src/main/java/com/example/corestudy/AppConfig.java
@@ -1,7 +1,9 @@
 package com.example.corestudy;
 
+import com.example.corestudy.discount.DiscountPolicy;
 import com.example.corestudy.discount.FixDiscountPolicy;
 import com.example.corestudy.discount.RateDiscountPolicy;
+import com.example.corestudy.member.MemberRepository;
 import com.example.corestudy.member.MemberService;
 import com.example.corestudy.member.MemberServiceImpl;
 import com.example.corestudy.member.MemoryMemberRepository;
@@ -11,13 +13,21 @@ import com.example.corestudy.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
+    }
+
+    private static DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
