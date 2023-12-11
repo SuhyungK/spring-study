1e36f8a150fe7afaa42d663ba7dbe49e79268e8f
SuhyungK <dook4966@gmail.com>
2023-12-11 15:06:45 +0900

    섹션3. AppConfig 스프링으로 전환하기

--- core-study/src/main/java/com/example/corestudy/AppConfig.java
+++ core-study/src/main/java/com/example/corestudy/AppConfig.java
@@ -9,17 +9,23 @@ import com.example.corestudy.member.MemberServiceImpl;
 import com.example.corestudy.member.MemoryMemberRepository;
 import com.example.corestudy.order.OrderService;
 import com.example.corestudy.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     private static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
@@ -27,6 +33,7 @@ public class AppConfig {
         );
     }
 
+    @Bean
     private static DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
