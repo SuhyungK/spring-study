8a74a08da9adc6b6740fd027c33161caeacb34c1
SuhyungK <dook4966@gmail.com>
2023-12-11 17:27:00 +0900

    섹션5. 스프링 컨테이너 싱글톤 테스트

---
+++ core-study/src/test/java/com/example/corestudy/singleton/SingletonTest.java
@@ -0,0 +1,21 @@
+package com.example.corestudy.singleton;
+
+import com.example.corestudy.AppConfig;
+import com.example.corestudy.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SingletonTest {
+    @Test
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(memberService1)
+                  .isSameAs(memberService2);
+    }
+}
