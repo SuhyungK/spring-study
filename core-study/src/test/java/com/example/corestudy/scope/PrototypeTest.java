ba28d2b09b4cd200b6ec94a4af837202ff4ada38
SuhyungK <dook4966@gmail.com>
2023-12-11 21:01:20 +0900

    섹션9. 프로토타입 스코프 테스트

---
+++ core-study/src/test/java/com/example/corestudy/scope/PrototypeTest.java
@@ -0,0 +1,35 @@
+package com.example.corestudy.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+    @Test
+    public void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        assertThat(prototypeBean1)
+                .isNotSameAs(prototypeBean2);
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
